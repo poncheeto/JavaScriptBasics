af60b31cb5ec1302df94d4d3dc1eaa8be9d8ad48
Poncho <jplyles1212@gmail.com>
2021-03-16 21:23:23 -0700

    Add nesting for loops

--- basicJS.js
+++ basicJS.js
@@ -1,6 +1,22 @@
+// Iterate through array with for loop
 var myArr = [ 2, 3, 4, 5, 6]
 
 var total = 0;
 for (var i = 0; i < myArr.length; i++) {
   total += myArr[i];
 }
+
+// Nesting for loops
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1,2],[3,4],[5,6,7]]);
